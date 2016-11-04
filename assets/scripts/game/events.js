75d94178f710159fef9a84e58df73b66a953be2c
jbuzzanco <jcbuzzanco@gmail.com>
2016-11-03 21:49:18 -0400

    created game logic win condition
    does not yet show who wins, not on the game board in html but
    the array board indexes

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -6,14 +6,32 @@ const ui = require('./ui');
 const store = require('../store');
 const getFormFields = require('../../../lib/get-form-fields');
 
+
+
+let winner = "x" || "o";
+let board = ['','','','','','','','','',];
+
+
+let checkWins = function() {
+  console.log(board);
+  console.log(board[0] === board[1]);
+  console.log(board[2] === board[0]);
+  console.log(!!board[0])
+ if (board[0] !== '' && board[0] === board[1] && board[2] === board[0]) {
+   console.log(store.turn + " Wins");
+ }
+};
+
 const addHandlers = function() {
-$('.box').on('click', function(){
-    store.turn = store.turn === "x"?"o":"x";
-    $(this).html(store.turn);
+  $('.box').on('click', function(){
+      store.turn = store.turn === "x"?"o":"x";
+      $(this).html(store.turn);
+      checkWins();
   });
 };
 
 
 module.exports = {
-  addHandlers
+  addHandlers,
+  checkWins,
 };
