aaee7b447c34325563886e9ef01a7005a1b08b7b
jbuzzanco <jcbuzzanco@gmail.com>
2016-11-08 18:37:57 -0500

    Game logic displaying in console
    got all the win conditions to display in the console with click handler.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -18,11 +18,37 @@ let checkWins = function() {
   console.log(boardArray[2] === boardArray[0]);
   console.log(!!boardArray[0]);
 
-//  if (board[0] !== '' && board[0] === board[1] && board[2] === board[0]) {
-//    console.log(store.turn + " Wins");
-//  }
+// horizontal wins
+ if (boardArray[0] !== '' && boardArray[0] === boardArray[1] && boardArray[2] === boardArray[0]) {
+   console.log(store.turn + " Wins");
+ }
+  else if (boardArray[3] !== '' && boardArray[3] === boardArray[4] && boardArray[5] === boardArray[3]) {
+     console.log(store.turn + " Wins");
+  }
+  else if (boardArray[6] !== '' && boardArray[6] === boardArray[7] && boardArray[8] === boardArray[6]) {
+   console.log(store.turn + " Wins");
+ }
+ //vertical Wins
+  else if (boardArray[0] !== '' && boardArray[0] === boardArray[3] && boardArray[6] === boardArray[0]) {
+  console.log(store.turn + " Wins");
+  }
+  else if (boardArray[1] !== '' && boardArray[1] === boardArray[4] && boardArray[7] === boardArray[1]) {
+   console.log(store.turn + " Wins");
+  }
+  else if (boardArray[2] !== '' && boardArray[2] === boardArray[5] && boardArray[8] === boardArray[2]) {
+   console.log(store.turn + " Wins");
+  }
+  // diagonal Wins
+  else if (boardArray[0] !== '' && boardArray[0] === boardArray[4] && boardArray[8] === boardArray[0]) {
+   console.log(store.turn + " Wins");
+  }
+  else if (boardArray[2] !== '' && boardArray[2] === boardArray[4] && boardArray[6] === boardArray[2]) {
+   console.log(store.turn + " Wins");
+  }
+
 
  };
+
  const gameResolutionXorO = function() {
    let victor;
    if (boardArray[0] === 'x' && boardArray[1] === 'x' && boardArray[2] === 'x') {
@@ -38,6 +64,12 @@ const addHandlers = function() {
   $('.box').on('click', function(){
       store.turn = store.turn === "x"?"o":"x";
       $(this).html(store.turn);
+      // change board array to reflect changes in HTML
+      store.win = store.win === ("x" && "x" && "x") || ("o" && "o" && "o");
+      let index = $(this).data('index');
+      // find the index of the box clicked --jQuery to reference the data-index in html
+      // set board array of that index to store.turn
+      boardArray[index] = store.turn;
       checkWins();
   });
 };
