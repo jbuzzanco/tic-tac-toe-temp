1d1d195067b8e6c8568d95a27fdf94c49582b3d1
jbuzzanco <jcbuzzanco@gmail.com>
2016-11-21 20:01:47 -0500

    attempting deployment
    following deployment fix issue #355

--- dependencies.js
+++ dependencies.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(25);
+	module.exports = __webpack_require__(27);
 
 
 /***/ },
@@ -10332,7 +10332,7 @@
 
 /***/ },
 
-/***/ 25:
+/***/ 27:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
